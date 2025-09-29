6e8298ea63ba6797c8ac965d5c4c3a6887f18128
Pat-rickJoyce <patrick.joyce@lantanagroup.com>
2025-09-29 12:53:07 -0600

    Animal Bites Template Added

--- js/ui.clean.v8_1.refresh.js
+++ js/ui.clean.v8_1.refresh.js
@@ -125,6 +125,7 @@ const templateSelect = create('select', {
 });
 templateSelect.innerHTML = `
   <option value="">📋 Load Template...</option>
+  <option value="AnimalBites.json">Animal Bites</option>
   <option value="Spina Bifida (Disorders) (ICD10CM).json">Spina Bifida</option>
   <option value="NAS.json">Neonatal Abstinence Syndrome</option>
   <option value="SB.json">Cervical Spina Bifida</option>
